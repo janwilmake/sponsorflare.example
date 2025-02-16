130d3c3124810c2feb4010a1de81d01f6b423282
Jan Wilmake <jan@wilmake.com>
2025-02-16 12:35:03 +0100

    improvements

--- main.ts
+++ main.ts
@@ -4,7 +4,16 @@ export default {
   fetch: async (request: Request, env: Env) => {
     const sponsorflare = await middleware(request, env);
     if (sponsorflare) return sponsorflare;
-    const sponsor = await getSponsor(request, env);
+
+    const sponsor = await getSponsor(request, env, {
+      // charges 1 cent
+      charge: 1,
+    });
+
+    if (!sponsor.charged) {
+      return new Response("Payment required", { status: 402 });
+    }
+
     // do something
     return new Response("Your stuff here");
   },
